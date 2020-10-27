d2f35ee46275db999c124a25582a6d86ebaef389
Anwesh43 <anweshthecool0@gmail.com>
2020-10-27 07:07:37 +0530

    adding Canvas extension methods

--- crossarcexpanderview/src/main/java/com/anwesh/uiprojects/crossarcexpanderview/CrossArcExpanderView.kt
+++ crossarcexpanderview/src/main/java/com/anwesh/uiprojects/crossarcexpanderview/CrossArcExpanderView.kt
@@ -13,10 +13,12 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.RectF
 
-val parts : Int = 8
+val lines : Int = 4
+val parts : Int = 2 * lines
 val scGap : Float = 0.02f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 2.9f
+val rFactor : Float = 8.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val colors : Array<Int> = arrayOf(
@@ -35,3 +37,35 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawCrossArcExpander(i : Int, scale : Float, w : Float, h : Float, paint : Paint) {
+    val r : Float = Math.min(w, h) / rFactor
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(i, parts)
+    val sf2 : Float = sf.divideScale(lines + i, parts)
+    val deg : Float = rot * sf2
+    save()
+    rotate(rot * i)
+    drawLine(0f, 0f, size * sf1, size * sf1, paint)
+    drawArc(RectF(-r, -r, r, r), -deg / 2, deg, true, paint)
+    restore()
+}
+
+fun Canvas.drawCrossArcExpanders(scale : Float, w : Float, h : Float, paint : Paint) {
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..3) {
+        drawCrossArcExpander(j, scale, w, h, paint)
+    }
+    restore()
+}
+
+fun Canvas.drawCAENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawCrossArcExpanders(scale, w, h, paint)
+}
