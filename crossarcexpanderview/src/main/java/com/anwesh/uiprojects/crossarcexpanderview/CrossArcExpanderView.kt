ca3494336454fc544743c8f4ffde4c8ab3f0f909
Anwesh43 <anweshthecool0@gmail.com>
2020-10-27 07:34:46 +0530

    adding CAENode class

--- crossarcexpanderview/src/main/java/com/anwesh/uiprojects/crossarcexpanderview/CrossArcExpanderView.kt
+++ crossarcexpanderview/src/main/java/com/anwesh/uiprojects/crossarcexpanderview/CrossArcExpanderView.kt
@@ -132,4 +132,45 @@ class CrossArcExpanderView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CAENode(var i : Int, val state : State = State()) {
+
+        private var prev : CAENode? = null
+        private var next : CAENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CAENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCAENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CAENode {
+            var curr : CAENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
