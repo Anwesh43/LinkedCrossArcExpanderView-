46429e6fd52025d59be5feaf9758841d5a7b8d8b
Anwesh43 <anweshthecool0@gmail.com>
2020-10-27 06:46:38 +0530

    adding constants and libraries

--- crossarcexpanderview/src/main/java/com/anwesh/uiprojects/crossarcexpanderview/CrossArcExpanderView.kt
+++ crossarcexpanderview/src/main/java/com/anwesh/uiprojects/crossarcexpanderview/CrossArcExpanderView.kt
@@ -3,3 +3,30 @@ package com.anwesh.uiprojects.crossarcexpanderview
 /**
  * Created by anweshmishra on 27/10/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val parts : Int = 8
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#03A9F4",
+        "#FF9800",
+        "#3F51B5",
+        "#009688"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val rot : Float = 90f
+val start : Float = 45f
